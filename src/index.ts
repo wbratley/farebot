cf335e9726cbf29e47464e38b54fddbc940d733c
Wayne B <soulnemesis@gmail.com>
2020-11-21 23:50:07 +0000

    refactor and make prettier

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,38 @@ function getFare(warpCells: number): number{
     const unroundedFare = (warpCells * 13000 + 150000) * 1.225;
     const remainder = 25000 - unroundedFare % 25000;
     return unroundedFare + remainder;
+}
+
+
+function showCommandsCommand(args: string[]): discord.MessageEmbed{
+    return new discord.MessageEmbed()
+    .setTitle('Farebot Commands:')
+    .setColor(0x00ff00)
+    .setDescription(
+        `**!commands** - Display this message
+        **!warp-fare** - Calculate one way fare for travel between planets
+        **!warp-fare-rt** - Calculate round trip fare for travel between planets`);
 
 }
 
-function warpFareCommand(args: string[]): string{
-    if(args.length == 0) 
-    return
-        `One way fare calculations (use !warp-fare-rt for round trip):
-        **!warp-fare** <num warp cells>
-        **!warp-fare** <weight (t)> <distance (su)>
-        **!warp-fare** <ship weight (t)> <cargo weight (t)> <distance (su)>`;
+function warpFareCommand(args: string[]): discord.MessageEmbed{
+
+    if(args.length == 0) {
+        return new discord.MessageEmbed()
+            .setTitle('One way fare calculations:')
+            .setColor(0xff0000)
+            .setDescription(
+                `**!warp-fare** <num warp cells>
+                **!warp-fare** <weight (t)> <distance (su)>
+                **!warp-fare** <ship weight (t)> <cargo weight (t)> <distance (su)>`);
+    }
 
     if(args.length == 1) {
         const fare = getFare(parseInt(args[0]));
-       return "Fare : " + fare + "ħ";    
+        return new discord.MessageEmbed()
+            .setTitle('Fare:')
+            .setColor(0x0000ff)
+            .setDescription("Fare : " + fare + "ħ");
     }
 
     if (args.length == 2) {
@@ -28,7 +46,10 @@ function warpFareCommand(args: string[]): string{
         const distance = parseFloat(args[1]);
         const cells = Math.ceil(weight * distance * 0.00025);
         const fare = getFare(cells);
-        return "Fare : " + fare + "ħ";
+        return new discord.MessageEmbed()
+            .setTitle('Fare:')
+            .setColor(0x0000ff)
+            .setDescription("Fare : " + fare + "ħ");
     }
 
     if (args.length == 3) {
@@ -37,25 +58,34 @@ function warpFareCommand(args: string[]): string{
         const distance = parseFloat(args[2]);
         const cells = Math.ceil((weightShip + weightCargo) * distance * 0.00025);
         const fare = getFare(cells);
-        return "Fare : " + fare + "ħ";
+        return new discord.MessageEmbed()
+            .setTitle('Fare:')
+            .setColor(0x0000ff)
+            .setDescription("Fare : " + fare + "ħ");
     }
 }
 
-function sendResponse(message: discord.Message, response: string){
-    message.channel.send(">>> " + response + " >>>");
+function sendResponse(message: discord.Message, response: discord.MessageEmbed){
+    message.channel.send(response);
 }
 
-function warpFareRoundTripCommand(args: string[]): string{
-    if(args.length == 0) 
-       return
-            `Round trip fare calculations:
-            **!warp-fare-rt** <num warp cells>
+function warpFareRoundTripCommand(args: string[]): discord.MessageEmbed{
+    if(args.length == 0) {
+        return new discord.MessageEmbed()
+        .setTitle('Round trip fare calculations:')
+        .setColor(0xff0000)
+        .setDescription(
+            `**!warp-fare-rt** <num warp cells>
             **!warp-fare-rt** <ship weight (t)> <distance (su)>
-            **!warp-fare-rt** <ship weight (t)> <cargo weight (t)> <distance (su)>`;
+            **!warp-fare-rt** <ship weight (t)> <cargo weight (t)> <distance (su)>`);
+    }
 
     if(args.length == 1) {
         const fare = getFare(parseInt(args[0])*2);
-        return "Fare : " + fare + "ħ";   
+        return new discord.MessageEmbed()
+            .setTitle('Fare:')
+            .setColor(0x0000ff)
+            .setDescription("Fare : " + fare + "ħ"); 
     }
 
     if(args.length == 2) {
@@ -64,7 +94,10 @@ function warpFareRoundTripCommand(args: string[]): string{
         const distance = parseFloat(args[1]);
         const cells = Math.ceil(weight * distance * 0.00025);
         const fare = getFare(cells*2);
-        return "Fare : " + fare + "ħ";
+        return new discord.MessageEmbed()
+            .setTitle('Fare:')
+            .setColor(0x0000ff)
+            .setDescription("Fare : " + fare + "ħ"); 
     }
 
     if (args.length == 3) {
@@ -74,26 +107,32 @@ function warpFareRoundTripCommand(args: string[]): string{
         const cellsThere = Math.ceil((weightShip + weightCargo) * distance * 0.00025);
         const cellsBack = Math.ceil((weightShip) * distance * 0.00025);
         const fare = getFare(cellsThere + cellsBack);
-        return "Fare : " + fare + "ħ";
+        return new discord.MessageEmbed()
+            .setTitle('Fare:')
+            .setColor(0x0000ff)
+            .setDescription("Fare : " + fare + "ħ"); 
     }
 }
 
 client.on("message", function(message : discord.Message){
-    if(message.author.bot) return;
-    if(!message.content.startsWith(prefix)) return;
+
+     if(message.author.bot) return;
+     if(!message.content.startsWith(prefix)) return;
 
     const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ').filter(e => e!=null);
+    const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
 
-    let response = null;
-    switch(command){
-        case 'warp-fare': response = warpFareCommand(args); break;
-        case 'warp-fare-rt': response = warpFareRoundTripCommand(args); break;
-    }
+    let response : discord.MessageEmbed = null;
+
+    if(command == 'commands' || command == 'help')  response = showCommandsCommand(args);
+    if(command == 'warp-fare')  response = warpFareCommand(args);
+    if(command == 'warp-fare-rt')  response = warpFareRoundTripCommand(args);
+
+   
 
     if(response != null)
-        sendResponse(message, response);
+         sendResponse(message, response);
 })
 
 client.login(process.env.BOT_TOKEN);
